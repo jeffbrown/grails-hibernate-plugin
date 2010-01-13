403ab91b181023b7e502fca34223ae89ac8d9fde
brownj <brownj@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2010-01-13 18:45:10 +0000

    set version number to 1.3.0.BUILD-SNAPSHOT
    git-svn-id: https://svn.codehaus.org/grails/trunk/grails-plugins/grails-hibernate/trunk@9264 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- HibernateGrailsPlugin.groovy
+++ HibernateGrailsPlugin.groovy
@@ -31,7 +31,7 @@ class HibernateGrailsPlugin {
 	def description = "A plugin that provides integration between Grails and Hibernate through GORM"
 
 	
-    def version = "1.2.1.SNAPSHOT"
+    def version = "1.3.0.BUILD-SNAPSHOT"
     def documentation = "http://grails.org/doc/$version"
 
     def pluginExcludes = [
