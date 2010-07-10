443d5ced6907177e57171e4290fb5e174d0afe00
Jeff Brown <jeff@jeffandbetsy.net>
2010-07-10 05:01:47 -0600

    set version number to 1.2.4.BUILD-SNAPSHOT

--- HibernateGrailsPlugin.groovy
+++ HibernateGrailsPlugin.groovy
@@ -1,4 +1,4 @@
-    /*
+4    /*
  * Copyright 2004-2005 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,7 +31,7 @@ class HibernateGrailsPlugin {
 	def description = "A plugin that provides integration between Grails and Hibernate through GORM"
 
 	
-    def version = "1.2.3"
+    def version = "1.2.4.BUILD-SNAPSHOT"
 	def grailsVersion = "1.2 > *"
     def documentation = "http://grails.org/doc/$version"
 
