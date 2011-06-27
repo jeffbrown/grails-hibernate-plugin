6adcc06b476ef183b2a65f2935ed7dc82c8845f5
Graeme Rocher <graemerocher@Graeme-Rochers-iMac.local>
2011-06-27 16:37:53 +0200

    updated excludes and version info

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
+
 grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
@@ -10,10 +11,10 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 	  	compile( 'org.hibernate:hibernate-core:3.6.1.Final' ){
-			excludes 'slf4j-api'
+			excludes 'slf4j-api', 'commons-collections'
 		}
 	    compile( 'org.hibernate:hibernate-validator:4.1.0.Final' ) {
-			excludes 'slf4j-api'
+			excludes 'slf4j-api', 'commons-collections'
 		}
 
 		runtime 'javassist:javassist:3.11.0.GA'
@@ -22,14 +23,18 @@ grails.project.dependency.resolution = {
 			excludes 'xml-apis'
 		}				
 		runtime( 'org.hibernate:hibernate-ehcache:3.6.1.Final' ) {
-			excludes 'ehcache', 'hibernate-core'
+			excludes 'ehcache', 'hibernate-core','slf4j-api', 'commons-collections'
 		}
 		
     }
 
 	plugins {
-		build( ":release:1.0.0.M2" ) {
+		build( ":release:1.0.0.M3" ) {
 			export = false
 		}
+		
+		build( ":svn:1.0.0.M1" ) {
+			export = false
+		}		
 	}
 }
