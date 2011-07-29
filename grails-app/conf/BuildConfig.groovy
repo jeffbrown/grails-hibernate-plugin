8deed625761ede3230aa82da2cf8b287c7beb89a
Graeme Rocher <graeme.rocher@gmail.com>
2011-07-29 11:28:29 +0200

    use 1.0 rc2 of release to provide better exclusion rules in the POM

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-
+grails.project.work.dir="target/work"
 grails.project.dependency.resolution = {
 
     inherits "global"
@@ -14,28 +14,29 @@ grails.project.dependency.resolution = {
 
     dependencies {
         compile('org.hibernate:hibernate-core:3.6.1.Final') {
-            excludes 'slf4j-api', 'commons-collections'
+			exclude group:'commons-collections', name:'commons-collections'
+			exclude group:'org.slf4j', name:'slf4j-api'			
         }
         compile('org.hibernate:hibernate-validator:4.1.0.Final') {
-            excludes 'slf4j-api', 'commons-collections'
+			exclude group:'commons-collections', name:'commons-collections'
+			exclude group:'org.slf4j', name:'slf4j-api'			
         }
 
         runtime 'javassist:javassist:3.11.0.GA'
         runtime 'antlr:antlr:2.7.6'
         runtime('dom4j:dom4j:1.6.1') {
-            excludes 'xml-apis'
+			exclude group:'xml-apis', name:'xml-apis'			
         }
         runtime('org.hibernate:hibernate-ehcache:3.6.1.Final') {
-            excludes 'ehcache', 'hibernate-core','slf4j-api', 'commons-collections'
+			exclude group:'commons-collections', name:'commons-collections'
+			exclude group:'org.slf4j', name:'slf4j-api'			
+			exclude group:'org.hibernate', name:'hibernate-core'						
+			exclude group:'net.sf.ehcache', name:'ehcache'			
         }
     }
 
     plugins {
-        build(":release:1.0.0.M3") {
-            export = false
-        }
-
-        build(":svn:1.0.0.M1") {
+        build(":release:1.0.0.RC2") {
             export = false
         }
     }
